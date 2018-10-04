4a323ff93760229f7756c2141e245a956fc7b098
Josh McArthur <joshua.mcarthur@gmail.com>
2018-10-04 21:05:19 +1300

    Add exercise tests for 'questionnaire' exercise

--- questionnaire/exercise_test.rb
+++ questionnaire/exercise_test.rb
@@ -27,12 +27,69 @@ class QuestionnaireTest < ActiveSupport::TestCase
   self.use_transactional_tests = true
 
   # * A new questionnaire can be created
+  def test_creates_questionnaire
+    questionnaire = build_questionnaire
+    assert questionnaire.persisted?, 'Questionnaire was created'
+  end
+
   # * Questions can be added to the questionnaire
+  def test_adds_questions_to_questionnaire
+    questionnaire = build_questionnaire
+    3.times { questionnaire.questions << build_question }
+    assert_equal questionnaire.questions.count, 3, 'Questionnaire has questions'
+  end
+
   # * Answers can be added to each question
+  def test_adds_answers_to_question
+    questionnaire = build_questionnaire
+    questionnaire.questions << question = build_question
+    3.times { question.answers << build_answer }
+    assert_equal question.answers.count, 3, 'Question has answers'
+  end
+
   # * A user can answer questions on the questionnaire
-  # * A list of questions can be accessed on the questionnaire
-  # * A list of available answers can be accessed for each question
-  # * A list of response can be accessed for each question
-  # * (Semi-extension): A list of responses can be accessed across the
-  #   entire questionnaire
+  def test_question_can_be_answered
+    questionnaire = build_questionnaire
+    questionnaire.questions << question = build_question
+    question.answers << answer = build_answer
+    response = create_response(answer: answer)
+    assert_equal answer.responses, [response], 
+                 'Answer has expected responses'
+  end
+
+  def test_responses_can_be_accessed_for_a_question
+    questionnaire = build_questionnaire
+    questionnaire.questions << question = build_question
+    3.times { question.answers << build_answer }
+    10.times { create_response(answer: question.answers.sample) }
+    assert_equal question.responses.count, 10, 
+                 'Question has expected responses'
+  end
+
+  def test_responses_can_be_accessed_for_a_questionnaire
+    questionnaire = build_questionnaire
+    questionnaire.questions << question = build_question
+    3.times { question.answers << build_answer }
+    10.times { create_response(answer: question.answers.sample) }
+    assert_equal questionnaire.responses.count, 10, 
+                 'Questionnaire has expected responses'
+  end
+
+  private
+
+  def build_questionnaire
+    Questionnaire.create!(title: 'Test Questionnaire')
+  end
+
+  def build_question
+    Question.new(title: "Question #{rand(1..999)}")
+  end
+
+  def build_answer
+    Answer.new(label: "Answer #{rand(1..999)}")
+  end
+
+  def create_response(attrs = {})
+    Response.create!(attrs)
+  end
 end
